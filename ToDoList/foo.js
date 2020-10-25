21a86abb8d4c6d95e8699e01449cbb3e921de56e
Asadbek-Dev <atmdt1101@gmail.com>
2020-10-25 14:24:46 +0300

    inputni formga kiritdim endi enterni bossak boladi

--- ToDoList/foo.js
+++ ToDoList/foo.js
@@ -1,3 +1,27 @@
+// var liElement = document.getElementsByTagName("LI");
+// for (var i = 0; i < liElement.length; i++) {
+//   var span = document.createElement("SPAN");
+//   var txt = document.createTextNode("\u00D7");
+//   span.className = "close";
+//   span.appendChild(txt);
+//   liElement[i].appendChild(span);
+// }
+
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function() {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
+
+var lists = document.querySelector('ul');
+lists.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
 // LocalStorage dan olish
 if (window.localStorage.length) {
   for (var i = 1; i <= window.localStorage.length; i++) {
@@ -10,24 +34,26 @@ if (window.localStorage.length) {
     console.log (res);
   }
 }
+
 // localStorage ga yuklash
 function myFunction (event) {
   event.preventDefault ();
-  var lists = document.querySelector ('#lists');
-  var list = document.getElementsByClassName ('list');
-  var liElement = document.createElement ('LI');
-  var inputValue = document.querySelector ('#inputValue').value;
-  var text = document.createTextNode (inputValue);
+  let lists = document.querySelector ('#lists');
+  let list = document.getElementsByClassName ('list');
+  let liElement = document.createElement ('LI');
+  let inputValue = document.querySelector ('#inputValue').value;
+  let text = document.createTextNode (inputValue);
   if (!inputValue) {
     alert ("Iltimos to'ldiring");
   } else {
     window.localStorage.setItem (
-      window.localStorage.length ? window.localStorage.length + 1 : 1,
+      window.localStorage.length ? window.localStorage.length+1: 1,
       inputValue
     );
     liElement.appendChild (text);
     lists.appendChild (liElement);
     document.querySelector ('#inputValue').value = '';
+
   }
   console.log (lists);
 }
